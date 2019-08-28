603d9cdff395b3b7321f9fd598d68d9ee077335a
Uzair Ali <uzaira960@gmail.com>
2019-08-28 11:36:29 +0100

    Added md_link_checker to gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,7 @@
 module.exports = function(grunt) {
 
+  const checkLinks = require('md-link-checker')
+
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
 
@@ -20,11 +22,44 @@ module.exports = function(grunt) {
       src: [
         'content/en/**/*.md'
       ]
+    },
+
+    md_link_checker: {
+      src: ['content/en/**/*.md']
     }
   });
 
   grunt.loadNpmTasks('grunt-markdownlint');
+  grunt.registerTask('default', ['md_link_checker']);
 
-  grunt.registerTask('default', ['markdownlint']);
+  grunt.registerMultiTask('md_link_checker', 'Check links in markdown even github private ones', function () {
+    const done = this.async();
+    const options = this.options({
+        tokenFilePath: undefined,
+    });
 
+  this.files.forEach(function (f) {
+            const src = f.src.filter(function (path) {
+                if (!grunt.file.exists(path)) {
+                    grunt.log.warn(`File ${path} not found.`);
+                    return false;
+                } else {
+                     return true;
+                }
+            });
+            const config = {
+                files: src,
+                tokenFilePath: options.tokenFilePath
+            };
+
+            return checkLinks(config).then(function (data) {
+                grunt.log.ok('No dead links found');
+                return done();
+            }).catch(function (data) {
+                if ()
+                data.forEach(dead => grunt.log.warn(`Link ${dead.deadlinks.join(', ')} in ${dead.filename} not found.`));
+                return done(false);
+            });
+        });
+    });
 };
